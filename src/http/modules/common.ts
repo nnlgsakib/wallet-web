d6929d8fd328f562ce3bf97287a6728726531636
Jason Chen <447462131@qq.com>
2024-02-22 16:26:36 +0800

    feat: modif the domain of limino.com

--- src/http/modules/common.ts
+++ src/http/modules/common.ts
@@ -1,104 +1,113 @@
-import { httpGet, httpPost } from '../request'
+import { httpGet, httpPost } from "../request";
 
-export const isProduct = process.env.VUE_APP_NODE_ENV == 'production' || process.env.VUE_APP_NODE_ENV == 'test' ? true : false
-const api = isProduct ? '' : '/createExchange'
+export const isProduct =
+  process.env.VUE_APP_NODE_ENV == "production" ||
+  process.env.VUE_APP_NODE_ENV == "test"
+    ? true
+    : false;
+const api = isProduct ? "" : "/createExchange";
 
-const exchangeApi = isProduct ?'':'/exchangeApi'
-const exchansApi = isProduct ? '' : '/exchans'
+const exchangeApi = isProduct ? "" : "/exchangeApi";
+const exchansApi = isProduct ? "" : "/exchans";
 // Test url
-export const snftUrl = isProduct ? '' : ''
+export const snftUrl = isProduct ? "" : "";
 // Create an exchange with one click
 export const createExchange = (data: any) => {
-    return httpGet(`${api}/install/do_conf`, data)
-}
+  return httpGet(`${api}/install/do_conf`, data);
+};
 
 // Example Query whether the exchange is generated successfully
 export const is_install = (address: string) => {
-    return httpGet(`${api}/install/is_install`, { address: address.toLowerCase() },null)
-}
+  return httpGet(
+    `${api}/install/is_install`,
+    { address: address.toLowerCase() },
+    null
+  );
+};
 
 // One-click exchange to get the address
 export const getSysParams = (address: string, params = {}) => {
-    return httpGet(`${exchangeApi}/c${address.toLowerCase()}/v2/querySysParams`, params)
-}
+  return httpGet(
+    `${exchangeApi}/c${address.toLowerCase()}/v2/querySysParams`,
+    params
+  );
+};
 
 /**
  * Query exchange signature data
- * @param address 
- * @param params 
+ * @param address
+ * @param params
  * @returns boolean
  */
 export const getExchangeSig = (address: string, params = {}) => {
-    return httpGet(`${exchangeApi}/c${address.toLowerCase()}/v2/getExchangeSig`, params)
-}
-
-
+  return httpGet(
+    `${exchangeApi}/c${address.toLowerCase()}/v2/getExchangeSig`,
+    params
+  );
+};
 
 // Set System information Set one-click exchange data
 export const setExchangeSig = (address: string, params = {}) => {
-    return httpPost(`${exchangeApi}/c${address.toLowerCase()}/v2/setExchangeSig`, params)
-}
+  return httpPost(
+    `${exchangeApi}/c${address.toLowerCase()}/v2/setExchangeSig`,
+    params
+  );
+};
 
 // Get the smart contract address
 export const getContractAddress = () => {
-    return httpGet(`https://www.limino.com/upload/ERBPay.json?${new Date().getTime()}`)
-}
+  return httpGet(
+    `https://wallet.erbie.io/upload/ERBPay.json?${new Date().getTime()}`
+  );
+};
 
 export const getConfiguration = () => {
-    return httpGet(`https://www.limino.com/upload/configuration.json?${new Date().getTime()}`)
-}
-
+  return httpGet(
+    `https://wallet.erbie.io/upload/configuration.json?${new Date().getTime()}`
+  );
+};
 
 // const contractApi = isProduct ? 'contractApi' : 'contractApi'
-const contractApi = isProduct ? 'contractApi' : 'contractApi'
-
+const contractApi = isProduct ? "contractApi" : "contractApi";
 
 // Query the account information of the specified address
 export const getAccountAddr = (address: string) => {
-    return httpGet(`${contractApi}/account/${address}`,{})
-}
-
+  return httpGet(`${contractApi}/account/${address}`, {});
+};
 
 // Get user NFT reward information  nftaward/v1/queryuseraward
 export const queryuseraward = (user_addr: string) => {
-    return httpPost(`${exchansApi}/nftaward/v1/queryuseraward`,{user_addr})
-}
-
+  return httpPost(`${exchansApi}/nftaward/v1/queryuseraward`, { user_addr });
+};
 
 /**
  * receive award /v1/receiveuseraward
  * award_type erb/snft/exchange
  * @param {user_addr}
  * @param {award_type}
- * @returns 
+ * @returns
  */
 export const receiveuseraward = (params: any) => {
-    return httpPost(`${exchansApi}/nftaward/v1/receiveuseraward`, params)
-}
-
-
-
-
-
+  return httpPost(`${exchansApi}/nftaward/v1/receiveuseraward`, params);
+};
 
 export interface CreatorData {
-    address: string
-    count: number
-    lastEpoch: string
-    lastNumber: number
-    lastTime: number
-    number: number
-    profit: string
-    reward: string
-    timestamp: number
+  address: string;
+  count: number;
+  lastEpoch: string;
+  lastNumber: number;
+  lastTime: number;
+  number: number;
+  profit: string;
+  reward: string;
+  timestamp: number;
 }
 // specifies the address to query the creator
 export const getCreator = (address: string): Promise<CreatorData> => {
-    return httpGet(`${contractApi}/creator/${address}`, {})
-}
-
+  return httpGet(`${contractApi}/creator/${address}`, {});
+};
 
 // specifies the ID to query the NFT period information of the system, including 16 collection information
 export const getPeriodById = (id: string): Promise<any> => {
-    return httpGet(`${snftUrl}/epoch/${id}`, {})
-}
+  return httpGet(`${snftUrl}/epoch/${id}`, {});
+};
